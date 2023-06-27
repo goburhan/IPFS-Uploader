e17bb426c764f2a58503c89b67de43b82250ddbc
goburhan <goburhann@gmail.com>
2023-06-27 17:43:14 +0300

    usercollections taking from connected wallet

--- src/hooks/useUserCollections.js
+++ src/hooks/useUserCollections.js
@@ -13,10 +13,7 @@ const useUserCollections = () => {
       redirect: "follow",
     };
 
-    fetch(
-      "http://groll2.eu-central-1.elasticbeanstalk.com/direct/0x8323B1A1057B76476eed77E98a35Ed7B26D50a7F/f_ownedTokenIds/0xA55Ec1Ccc5877A4A24D8D9435f8B3Ba196CB5D4a",
-      requestOptions
-    )
+    fetch(process.env.NEXT_PUBLIC_GET_COLLECTION + userAddress, requestOptions)
       .then((response) => response.json()) // if the response is a JSON object
       .then((result) => {
         setData(result);
@@ -24,7 +21,7 @@ const useUserCollections = () => {
       .catch((error) => {
         setError(error);
       });
-  }, []);
+  }, [userAddress]);
 
   return { data, error };
 };
